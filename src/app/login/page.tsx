428647d118ec0d738cde0e6279032608853998f8
Yul Jeon <dalbitstream@gmail.com>
2025-04-24 15:48:18 +0900

    fix: add default keyword

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,16 @@
-import {GoogleLoginButton} from "@/components/loginButton/googleLogin";
-import {KakaoLoginButton} from "@/components/loginButton/kakaoLogin";
+import { GoogleLoginButton } from '@/components/loginButton/googleLogin';
+import { KakaoLoginButton } from '@/components/loginButton/kakaoLogin';
 
-export function LoginPage() {
-    return (
-        <main className="min-h-screen flex items-center justify-center bg-[#131722] text-white px-4">
-            <div className="w-full max-w-md bg-[#1E222D] rounded-2xl p-8 space-y-6">
-                <h1 className="text-3xl font-bold text-center">로그인</h1>
-                <div className="flex flex-col space-y-4">
-                    <GoogleLoginButton/>
-                    <KakaoLoginButton/>
-                </div>
-            </div>
-        </main>
-    );
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[#131722] text-white px-4">
+      <div className="w-full max-w-md bg-[#1E222D] rounded-2xl p-8 space-y-6">
+        <h1 className="text-3xl font-bold text-center">로그인</h1>
+        <div className="flex flex-col space-y-4">
+          <GoogleLoginButton />
+          <KakaoLoginButton />
+        </div>
+      </div>
+    </main>
+  );
 }
