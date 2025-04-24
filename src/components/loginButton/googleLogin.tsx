4d35aaf80689ba8916c6394a24dfc08b2c13ab9f
Kimmy <kimsungrak00@gmail.com>
2025-04-24 14:33:29 +0900

    fix: 구글 컴포넌트 수정

--- src/components/loginButton/googleLogin.tsx
+++ src/components/loginButton/googleLogin.tsx
@@ -8,6 +8,7 @@ const GoogleLogin = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
