b95e50403bf41a5abe1ac33f8a22289fd2db2dc1
HwaEVE <kimsungrak00@gmail.com>
2025-04-24 14:54:31 +0900

    Feat/auth supabase (#3)
    * feat: 메인 페이지에 로그인 링크 추가
    
    * feat: Install the supabase package
    
    * feat: Write utility functions to create Supabase clients
    
    * feat: Hook up middleware
    
    * feat: Create a route handler for Auth confirmation
    
    * feat: 깃허브, 구글, 카톡 로그인 버튼 컴포넌트 구축
    
    * feat: signOutAction 구현
    
    * feat: 로그인 페이지 구현 - 디자인은 아직 미구현
    
    * fix: fix a route handler for Auth confirmation
    
    * fix: 구글 재로그인
    
    * fix: eslint uninstall
    
    * fix: 깃허브 삭제 및 로그인에서 깃허브 제거
    
    * fix: 구글 컴포넌트 수정
    
    * fix: 로그인 클라이언트를 서버로 변경
    
    * fix: 로그인의 유즈 서버 삭제

---
+++ src/components/loginButton/kakaoLogin.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+
+const Kakaologin = async () => {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "kakao",
+    options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
+    },
+  });
+
+  console.log(data);
+
+  if (error) {
+    console.log(error);
+  }
+};
+
+export function KakaoLoginButton() {
+  return (
+    <button type="button" onClick={Kakaologin} className="naver">
+      Kakao 로그인
+    </button>
+  );
+}
