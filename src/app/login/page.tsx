4fe3811b98ca3c0133f61ec174bd2b844c3f8874
Kimmy <kimsungrak00@gmail.com>
2025-04-24 13:44:48 +0900

    feat: 로그인 페이지 구현 - 디자인은 아직 미구현

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import {GitHubLoginButton} from "@/components/loginButton/gitHubLogin";
+import {GoogleLoginButton} from "@/components/loginButton/googleLogin";
+import {KakaoLoginButton} from "@/components/loginButton/kakaoLogin";
+
+export default function LoginPage() {
+    return (
+        <main className="min-h-screen flex items-center justify-center bg-[#131722] text-white px-4">
+            <div className="w-full max-w-md bg-[#1E222D] rounded-2xl p-8 space-y-6">
+                <h1 className="text-3xl font-bold text-center">로그인</h1>
+                <div className="flex flex-col space-y-4">
+                    <GitHubLoginButton/>
+                    <GoogleLoginButton/>
+                    <KakaoLoginButton/>
+                </div>
+            </div>
+        </main>
+    );
+}
