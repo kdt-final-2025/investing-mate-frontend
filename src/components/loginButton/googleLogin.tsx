b40b1481f590121696e04765a75c368274364695
Kimmy <kimsungrak00@gmail.com>
2025-04-24 14:17:07 +0900

    fix: 구글 재로그인

--- src/components/loginButton/googleLogin.tsx
+++ src/components/loginButton/googleLogin.tsx
@@ -6,9 +6,12 @@ const GoogleLogin = async () => {
   const supabase = createClient();
 
   const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
+    provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`,
+      queryParams: {
+        access_type: 'offline',
+        prompt: 'consent',
+      },
     },
   });
 
