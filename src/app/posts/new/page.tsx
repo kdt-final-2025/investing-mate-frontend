e7a200576835db5d4ad7552b185077453ce012ae
booungyi <ansdlsgur0123@gmail.com>
2025-04-29 14:50:44 +0900

    posts/new/page.tsx-> 서버 클라이언트 CreatePostForm.tsx->  use client 서버 , 유저 분리

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,44 +1,18 @@
-'use client';
+import { Metadata } from 'next';
+import CreatePostForm from '@/../src/components/posts/CreatePostForm';
 
-import { useState } from 'react';
-import { API_URL } from '@/env/constants';
-import { useRouter } from 'next/navigation';
+export const metadata: Metadata = {
+  title: '게시글 작성',
+  description: '새로운 게시글을 작성하는 페이지입니다.',
+};
 
 export default function CreatePostPage() {
-  const [postTitle, setPostTitle] = useState('');
-  const [content, setContent] = useState('');
-  const router = useRouter();
-
-  const handleCreate = async () => {
-    const res = await fetch(`${API_URL}/posts`, {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer your_token_here',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ postTitle, content, imageUrls: [] }),
-    });
-    if (res.ok) {
-      alert('게시글 작성 완료');
-      router.push('/posts');
-    } else {
-      alert('작성 실패');
-    }
-  };
-
   return (
-    <div>
-      <input
-        value={postTitle}
-        onChange={(e) => setPostTitle(e.target.value)}
-        placeholder="제목"
-      />
-      <textarea
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="내용"
-      />
-      <button onClick={handleCreate}>작성</button>
+    <div className="min-h-screen bg-[#131722] text-white container mx-auto p-6">
+      <div className="bg-[#1E222D] p-6 rounded-2xl shadow-md">
+        <h1 className="text-2xl font-bold mb-6">게시글 작성</h1>
+        <CreatePostForm />
+      </div>
     </div>
   );
 }
